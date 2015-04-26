b7bf772d6b5de35653e57451f2980ff1bd5fe692
tonyclyde <tonyclyde@gmail.com>
2015-04-26 13:18:50 -0500

    Initial commit of course project for coursera course: getdata-013

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # getDataProject
+
+## Summary
+Course project for Coursera course: getdata-013
+This project consists of one R file: run_analysis.R, and a folder of data for it to analyze.
+
+## Description
+
+### Step 1
+
+Loads the independent data files data/test/X_test.txt and data/train/X_train.txt which contain the bulk of the data.
+
+### Step 2
+
+Binds descriptive column names to the data sets (with pesky parentheses removed), loaded from: features.txt. Assumes the number of rows in this file is exactly the same as the number of columns in the test and train data.
+
+### Step 3
+
+Removes all columns from both sets which are not mean or std variables, again assumes this results in both data sets having the same number of columns.
+
+### Step 4
+
+Adds the subject ids to the data, loaded from: subject_test.txt and subject_train.txt respectively. Assumes the number of rows in the subject data matches the number of rows in their respective data sets.
+
+### Step 5
+
+Adds the activity ids to the data, loaded from: y_test.txt and y_train.txt respectively. Assumes the number of rows in the activity data matches the number of rows in their respective data sets.
+
+### Step 6
+
+Combine the two data sets together assuming identical column names, result in a data set with all rows from both of the originals.
+
+### Step 7
+
+Replace the activity_id column with an activity column containing the corresponding descriptive activity name from the activity_labels.txt file.
+
+### Step 8
+
+Create a second data set of column mean values, grouped by subject_id and activity, and write the result to the file: tidy_data.txt
---
+++ run_analysis.R
@@ -0,0 +1,41 @@
+# Load the main data files
+train_vals <- read.table("data/train/X_train.txt")
+test_vals <- read.table("data/test/X_test.txt")
+
+# bind descriptive column names (with pesky parentheses removed)
+col_name_data <- read.table("data/features.txt", colClasses = c("NULL", "character"))
+col_names <- sub("\\(\\)", "", col_name_data[[1]])
+names(train_vals) <- col_names
+names(test_vals) <- col_names
+
+# remove columns that are not mean or std variables
+mean_std_regex <- "^.*(mean|std)($|-.*$)"
+cols_to_keep <- grep(mean_std_regex, col_names)
+train_vals <- train_vals[, cols_to_keep]
+test_vals <- test_vals[, cols_to_keep]
+
+# add the subject ids to the data
+test_vals["subject_id"] <- read.table("data/test/subject_test.txt")
+train_vals["subject_id"] <- read.table("data/train/subject_train.txt")
+
+# add the activity ids to the data
+test_vals["activity_id"] <- read.table("data/test/y_test.txt")
+train_vals["activity_id"] <- read.table("data/train/y_train.txt")
+
+# merge the test and train data
+data <- merge(train_vals, test_vals, all = TRUE)
+
+# load the activity descriptions
+act_labels <- read.table("data/activity_labels.txt", col.names = c("activity_id", "activity"))
+
+# add the activity descriptions
+data <- merge(data, act_labels, by = "activity_id", all = TRUE)
+
+# remove "activity_id", leave descriptive "activity" column
+data <- mutate(data, activity_id = NULL)
+
+# create a second data set of column mean values, grouped by subject_id and activity
+data_avg <- data %>% group_by(subject_id, activity) %>% summarise_each(funs(mean), matches(mean_std_regex)) %>% arrange(subject_id, activity)
+
+# Finally, write the result file
+write.table(data_avg, file = "tidy_data.txt", row.name=FALSE)
